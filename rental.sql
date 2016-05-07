5e2f50fa189103478f558fa32f36d23fa8eb8d73
Nikho Sagala <Nikho Sagala@10.1.250.215>
2016-05-07 18:31:44 +0700

    Export new sql

--- rental.sql
+++ rental.sql
@@ -1,19 +1,20 @@
 -- phpMyAdmin SQL Dump
--- version 3.2.0.1
+-- version 4.5.1
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: Feb 22, 2016 at 10:26 PM
--- Server version: 5.1.37
--- PHP Version: 5.3.0
+-- Host: 127.0.0.1
+-- Generation Time: May 07, 2016 at 01:29 PM
+-- Server version: 10.1.10-MariaDB
+-- PHP Version: 5.6.19
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `rental`
@@ -25,12 +26,11 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 -- Table structure for table `mobil`
 --
 
-CREATE TABLE IF NOT EXISTS `mobil` (
+CREATE TABLE `mobil` (
   `plat` varchar(8) NOT NULL,
   `jenisMobil` varchar(20) NOT NULL,
   `harga` int(20) NOT NULL,
-  `Status` tinyint(1) NOT NULL,
-  PRIMARY KEY (`plat`)
+  `Status` tinyint(1) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
@@ -38,7 +38,9 @@ CREATE TABLE IF NOT EXISTS `mobil` (
 --
 
 INSERT INTO `mobil` (`plat`, `jenisMobil`, `harga`, `Status`) VALUES
-('D 1234 F', 'Innova', 300000, 1);
+('D 1234 F', 'Innova', 300000, 1),
+('AB8083CE', 'Bemo', 200000, 0),
+('D 46 AB', 'Innova', 315000, 0);
 
 -- --------------------------------------------------------
 
@@ -46,9 +48,10 @@ INSERT INTO `mobil` (`plat`, `jenisMobil`, `harga`, `Status`) VALUES
 -- Table structure for table `supir`
 --
 
-CREATE TABLE IF NOT EXISTS `supir` (
-  `idSupir` varchar(8) NOT NULL,
+CREATE TABLE `supir` (
+  `idSupir` int(8) NOT NULL,
   `namaSupir` varchar(20) NOT NULL,
+  `noTelp` varchar(12) NOT NULL,
   `hargaSupir` double NOT NULL,
   `status` tinyint(1) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
@@ -57,7 +60,35 @@ CREATE TABLE IF NOT EXISTS `supir` (
 -- Dumping data for table `supir`
 --
 
+INSERT INTO `supir` (`idSupir`, `namaSupir`, `noTelp`, `hargaSupir`, `status`) VALUES
+(1, 'Mizan', '0831231247', 150000, 0),
+(2, 'Mizan', '0831231247', 150000, 0);
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `mobil`
+--
+ALTER TABLE `mobil`
+  ADD PRIMARY KEY (`plat`);
+
+--
+-- Indexes for table `supir`
+--
+ALTER TABLE `supir`
+  ADD PRIMARY KEY (`idSupir`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `supir`
+--
+ALTER TABLE `supir`
+  MODIFY `idSupir` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
